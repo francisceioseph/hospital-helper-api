86d15fbc969031e286fa60041051e3fd9cc9b4b9
Francisco Souza <francisceioseph@gmail.com>
2019-01-12 21:29:40 -0300

    Add controller tests

--- test/controllers/pacient_controller_test.rb
+++ test/controllers/pacient_controller_test.rb
@@ -1,4 +1,36 @@
 require 'test_helper'
 
 class PacientControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @headers = { 'HTTP_ACCEPT': 'application/json' }
+  end
+  test 'can list all pacients' do
+    get pacients_url, headers: @headers
+    assert_response :success
+  end
+
+  test 'can retrieve a pacient' do
+    pacient = pacients(:one)
+    get pacients_url(pacient), headers: @headers
+    assert_response :success
+  end
+
+  test 'can create a pacient' do
+    post pacients_url, headers: @headers, params: {
+      pacient: {
+        profile_type: 'Pacient'
+      }
+    }
+    assert_response :success
+  end
+
+  test 'can update a pacient' do
+    pacient = pacients(:one)
+    patch "/pacients/#{pacient.id}", headers: @headers, params: {
+      pacient: {
+        profile_type: 'Pacient'
+      }
+    }
+    assert_response :success
+  end
 end
