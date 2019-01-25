9ef9156b11ef6522e3a50591dbbe0f1e4d66d8bb
Francisco Souza <francisceioseph@gmail.com>
2019-01-25 15:03:19 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
                 registration: 'signup'
               },
               controllers: {
-                sessions: 'sessions',
-                registrations: 'registrations'
+                sessions: 'users/sessions',
+                registrations: 'users/registrations'
               }
   
   get   '/doctors', to: 'doctors#index'
