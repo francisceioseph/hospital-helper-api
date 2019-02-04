b915fd088b5520ff2df2bc508300d94a179f91c2
Francisco Souza <francisco.souza@ifactory.com.br>
2019-02-04 11:04:14 -0300

    Add profile type to sessions controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,7 +6,7 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-    render json: resource.to_json(include: :profile)
+    render json: resource.to_json({ include: { profile: { methods: :profile_type }}})
   end
 
   def respond_to_on_destroy
