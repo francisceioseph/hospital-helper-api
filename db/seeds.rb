f8aa97eda18f3d584320089033e06ac6c49fc1aa
Francisco Souza <francisceioseph@gmail.com>
2019-02-06 12:59:04 -0300

    General fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -654,6 +654,11 @@ Permission.create!([
     id: 20,
     action_type: :view,
     resource_name: 'perfis-acesso'
+  },
+  {
+    id: 21,
+    action_type: :view,
+    resource_name: 'profile'
   }
 ])
 
@@ -668,7 +673,8 @@ Role.create!([
       { permission_id: 1 }, { permission_id: 2 }, { permission_id: 3 }, { permission_id: 4 }, { permission_id: 5 },
       { permission_id: 6 }, { permission_id: 7 }, { permission_id: 8 }, { permission_id: 9 }, { permission_id: 9 },
       { permission_id: 10 }, { permission_id: 11 }, { permission_id: 12 }, { permission_id: 13 }, { permission_id: 14 },
-      { permission_id: 15 }, { permission_id: 16 }, { permission_id: 17 }, { permission_id: 18 }, { permission_id: 19 }, { permission_id: 20 }
+      { permission_id: 15 }, { permission_id: 16 }, { permission_id: 17 }, { permission_id: 18 }, { permission_id: 19 },
+      { permission_id: 20 }, { permission_id: 21 }
     ]
   }
 ])
