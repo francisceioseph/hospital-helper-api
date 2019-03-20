d1e67fc6c9a4adee32f10eb8557c4bf671e96abb
Francisco Souza <francisco.souza@ifactory.com>
2019-03-20 10:41:48 -0300

    Remove content check for message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :sender, class_name: :User, foreign_key: 'user_id'
-  
-  validates_presence_of :content
     
   mount_uploaders :attachments, AttachmentUploader
 
