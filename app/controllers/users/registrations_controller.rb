b541dce0b15810c80c76e5180de9407c3d4759d9
Francisco Souza <francisceioseph@gmail.com>
2019-04-20 17:04:10 -0300

    Add new settings

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+
 class Users::RegistrationsController < Devise::RegistrationsController
+
   respond_to :json
 
   def create
@@ -13,7 +15,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def sign_up_params
-    Pretty.with_nested_attributes pretty_sign_up_params,
+    PrettyApi.with_nested_attributes pretty_sign_up_params,
                                   [profile: [:personal_datum]]
   end
 
